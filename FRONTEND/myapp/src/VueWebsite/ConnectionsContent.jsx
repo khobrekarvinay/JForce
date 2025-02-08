49b754a115280c27f4c3f45b1872344c95f09dde
khobrekarvinay <khobrekarvinay42@gmail.com>
2025-02-08 10:08:13 +0530

    update

--- FRONTEND/myapp/src/VueWebsite/ConnectionsContent.jsx
+++ FRONTEND/myapp/src/VueWebsite/ConnectionsContent.jsx
@@ -1,13 +1,207 @@
 import React from 'react';
-import { Typography, Box } from '@mui/material';
+import {
+    Box,
+    Typography,
+    Card,
+    CardContent,
+    Switch,
+    IconButton,
+    Grid,
+    Divider
+} from '@mui/material';
+import LinkIcon from '@mui/icons-material/Link';
+import DeleteIcon from '@mui/icons-material/Delete';
+
+const ConnectedAccounts = [
+    {
+        name: 'Google',
+        icon: 'https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg',
+        description: 'Calendar and contacts',
+        connected: true
+    },
+    {
+        name: 'Slack',
+        icon: 'https://upload.wikimedia.org/wikipedia/commons/d/d5/Slack_icon_2019.svg',
+        description: 'Communication',
+        connected: false
+    },
+    {
+        name: 'Github',
+        icon: 'https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png',
+        description: 'Manage your Git repositories',
+        connected: true
+    },
+    {
+        name: 'Mailchimp',
+        icon: 'https://cdn-icons-png.flaticon.com/512/5968/5968879.png',
+        description: 'Email marketing service',
+        connected: true
+    },
+    {
+        name: 'Asana',
+        icon: 'https://cdn.worldvectorlogo.com/logos/asana-1.svg',
+        description: 'Communication',
+        connected: false
+    }
+];
+
+const SocialAccounts = [
+    {
+        name: 'Facebook',
+        icon: 'https://upload.wikimedia.org/wikipedia/commons/b/b8/2021_Facebook_icon.svg',
+        status: 'Not Connected',
+        connected: false
+    },
+    {
+        name: 'Twitter',
+        icon: 'https://about.twitter.com/content/dam/about-twitter/x/brand-toolkit/logo-black.png.twimg.1920.png',
+        status: '@Pixinvent',
+        connected: true
+    },
+    {
+        name: 'Instagram',
+        icon: 'https://upload.wikimedia.org/wikipedia/commons/9/95/Instagram_logo_2022.svg',
+        status: '@Pixinvent',
+        connected: true
+    },
+    {
+        name: 'Dribbble',
+        icon: 'https://cdn.worldvectorlogo.com/logos/dribbble-icon-1.svg',
+        status: 'Not Connected',
+        connected: false
+    },
+    {
+        name: 'Behance',
+        icon: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c5/Behance_logo.svg/1024px-Behance_logo.svg.png',
+        status: 'Not Connected',
+        connected: false
+    }
+];
 
 function ConnectionsContent() {
-  return (
-    <Box>
-      <Typography variant="h6">Connections Content</Typography>
-      {/* Add connection content page elements here */}
-    </Box>
-  );
+    return (
+        <Box sx={{ p: 3 }}>
+            <Card sx={{ mb: 4 }}>
+                <CardContent>
+                    <Typography variant="h6" gutterBottom>
+                        Connected Accounts
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary" sx={{ mb: 3 }}>
+                        Display content from your connected accounts on your site
+                    </Typography>
+
+                    {ConnectedAccounts.map((account, index) => (
+                        <Box key={account.name}>
+                            <Box sx={{ 
+                                display: 'flex', 
+                                alignItems: 'center', 
+                                justifyContent: 'space-between',
+                                py: 2 
+                            }}>
+                                <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                                    <Box
+                                        component="img"
+                                        src={account.icon}
+                                        sx={{
+                                            width: 30,
+                                            height: 30,
+                                            mr: 2
+                                        }}
+                                        alt={account.name}
+                                    />
+                                    <Box>
+                                        <Typography variant="subtitle2">
+                                            {account.name}
+                                        </Typography>
+                                        <Typography variant="body2" color="text.secondary">
+                                            {account.description}
+                                        </Typography>
+                                    </Box>
+                                </Box>
+                                <Switch checked={account.connected} />
+                            </Box>
+                            {index < ConnectedAccounts.length - 1 && <Divider />}
+                        </Box>
+                    ))}
+                </CardContent>
+            </Card>
+
+            <Card>
+                <CardContent>
+                    <Typography variant="h6" gutterBottom>
+                        Social Accounts
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary" sx={{ mb: 3 }}>
+                        Display content from social accounts on your site
+                    </Typography>
+
+                    {SocialAccounts.map((account, index) => (
+                        <Box key={account.name}>
+                            <Box sx={{ 
+                                display: 'flex', 
+                                alignItems: 'center', 
+                                justifyContent: 'space-between',
+                                py: 2 
+                            }}>
+                                <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                                    <Box
+                                        component="img"
+                                        src={account.icon}
+                                        sx={{
+                                            width: 30,
+                                            height: 30,
+                                            mr: 2
+                                        }}
+                                        alt={account.name}
+                                    />
+                                    <Box>
+                                        <Typography variant="subtitle2">
+                                            {account.name}
+                                        </Typography>
+                                        <Typography variant="body2" color="text.secondary">
+                                            {account.status}
+                                        </Typography>
+                                    </Box>
+                                </Box>
+                                <Box>
+                                    {account.connected ? (
+                                        <IconButton 
+                                            size="small" 
+                                            sx={{ 
+                                                bgcolor: 'error.lighter',
+                                                color: 'error.main',
+                                                mr: 1,
+                                                '&:hover': {
+                                                    bgcolor: 'error.light'
+                                                }
+                                            }}
+                                        >
+                                            <DeleteIcon fontSize="small" />
+                                        </IconButton>
+                                    ) : (
+                                        <IconButton 
+                                            size="small" 
+                                            sx={{ 
+                                                bgcolor: 'grey.100',
+                                                color: 'grey.600',
+                                                mr: 1,
+                                                '&:hover': {
+                                                    bgcolor: 'grey.200'
+                                                }
+                                            }}
+                                        >
+                                            <LinkIcon fontSize="small" />
+                                        </IconButton>
+                                    )}
+                                </Box>
+                            </Box>
+                            {index < SocialAccounts.length - 1 && <Divider />}
+                        </Box>
+                    ))}
+                </CardContent>
+            </Card>
+        </Box>
+    );
 }
 
 export default ConnectionsContent;
--- FRONTEND/myapp/src/VueWebsite/Mainacc.jsx
+++ FRONTEND/myapp/src/VueWebsite/Mainacc.jsx
@@ -8,79 +8,95 @@ import ConnectionsContent from './ConnectionsContent';
 import Footer from './Footer';
 
 const StyledTab = styled(Tab)(({ theme }) => ({
-  fontSize: '14px',
-  fontWeight: 600,
-  textTransform: 'capitalize',
-  color: theme.palette.text.primary,
-  '&.Mui-selected': {
-    color: theme.palette.primary.main,
-  },
+    fontSize: '14px',
+    fontWeight: 600,
+    textTransform: 'capitalize',
+    color: theme.palette.text.primary,
+    '&.Mui-selected': {
+        color: theme.palette.primary.main,
+    },
 }));
 
 function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          {children}
-        </Box>
-      )}
-    </div>
-  );
+    const { children, value, index, ...other } = props;
+    return (
+        <div
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && (
+                <Box sx={{ p: 3 }}>
+                    {children}
+                </Box>
+            )}
+        </div>
+    );
 }
 
 function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-  };
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
 }
 
 function AccountPage() {
-  const [tabValue, setTabValue] = React.useState(0);
-  const theme = useTheme();
-  const isSidebarHidden = useMediaQuery(theme.breakpoints.down('lg'));
+    const [tabValue, setTabValue] = React.useState(0);
+    const theme = useTheme();
+    const isSidebarHidden = useMediaQuery(theme.breakpoints.down('lg'));
 
-  const handleChange = (event, newValue) => {
-    setTabValue(newValue);
-  };
+    const handleChange = (event, newValue) => {
+        setTabValue(newValue);
+    };
 
-  return (
-    <Box sx={{ width: '100%' }}>
-      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={tabValue} onChange={handleChange} aria-label="account tabs">
-          <StyledTab label="Account" {...a11yProps(0)} />
-          <StyledTab label="Security" {...a11yProps(1)} />
-          <StyledTab label="Billing & Plans" {...a11yProps(2)} />
-          <StyledTab label="Notifications" {...a11yProps(3)} />
-          <StyledTab label="Connections" {...a11yProps(4)} />
-        </Tabs>
-      </Box>
-
-      <TabPanel value={tabValue} index={0}>
-        <Account/>
-      </TabPanel>
-      <TabPanel value={tabValue} index={1}>
-        <SecurityContent />
-      </TabPanel>
-      <TabPanel value={tabValue} index={2}>
-        <BillingContent />
-      </TabPanel>
-      <TabPanel value={tabValue} index={3}>
-        <NotificationsContent />
-      </TabPanel>
-      <TabPanel value={tabValue} index={4}>
-        <ConnectionsContent />
-      </TabPanel>
-    </Box>
-  );
+    return (
+        <Box sx={{ display: 'flex', width: '100%', minHeight: '100vh' }}>
+            {/* Quick Access Bar --------------------------------*/}
+            {!isSidebarHidden && (
+                <Box sx={{ width: 220, backgroundColor: '#2f3349', height: '100vh', position: 'fixed', top: 0, left: 0, zIndex: 1, padding: 2 }}>
+                    <Box sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                        <img src="https://cdn.pixinvent.com/pi-assets/vuexy/admin-template/logo/logo.svg" alt="Logo" style={{ width: 40, height: 30 }} />
+                        <Typography sx={{ ml: 1, fontSize: '1.25rem', fontWeight: 600, color: '#fff', fontFamily: '"Public Sans", sans-serif', }}>
+                            Vuexy
+                        </Typography>
+                    </Box>
+                </Box>
+            )}
+            {/* Main Content ----------------------------------*/}
+            <Box sx={{ width: '100%', marginLeft: isSidebarHidden ? 0 : 40, padding: 2, flexGrow: 1 }}>
+                <Box sx={{ width: '100%' }}>
+                    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+                        <Tabs value={tabValue} onChange={handleChange} aria-label="account tabs">
+                            <StyledTab label="Account" {...a11yProps(0)} />
+                            <StyledTab label="Security" {...a11yProps(1)} />
+                            <StyledTab label="Billing & Plans" {...a11yProps(2)} />
+                            <StyledTab label="Notifications" {...a11yProps(3)} />
+                            <StyledTab label="Connections" {...a11yProps(4)} />
+                        </Tabs>
+                    </Box>
+                    <TabPanel value={tabValue} index={0}>
+                        <Account />
+                    </TabPanel>
+                    <TabPanel value={tabValue} index={1}>
+                        <SecurityContent />
+                    </TabPanel>
+                    <TabPanel value={tabValue} index={2}>
+                        <BillingContent />
+                    </TabPanel>
+                    <TabPanel value={tabValue} index={3}>
+                        <NotificationsContent />
+                    </TabPanel>
+                    <TabPanel value={tabValue} index={4}>
+                        <ConnectionsContent />
+                    </TabPanel>
+                </Box>
+            </Box>
+            <Footer />
+        </Box>
+    );
 }
 
 export default AccountPage;
